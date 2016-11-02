5195dacda9a78af0e53f7158859c74ff2758b0d9
hry712 <heruoyu2016@163.com>
2016-11-03 01:09:30 +0800

    the 2nd version has made a great improvement that I only use a simple method 'imageView.setImageURI(getIntent().getData())' which can satisfy both Sys Img Viewer's and custom ImgViewer's acquirement. Besides, this version saves the app res img to a local dir and use 'file://FilePath/FileName' as a param of Uri.parse() instead of using Bytes Stream or sending img files directly

--- app/src/main/java/com/jikexuyuan/imageviewer/MainActivity.java
+++ app/src/main/java/com/jikexuyuan/imageviewer/MainActivity.java
@@ -2,10 +2,22 @@ package com.jikexuyuan.imageviewer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.widget.Toast.makeText;
 
 public class MainActivity extends Activity {
 
@@ -15,17 +27,56 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 初始化按钮
+        // Initial the Open button widget
         btnOpenImg = (Button)findViewById(R.id.btn_openImg);
+        SaveImgToLocalPath(R.drawable.test,
+                ViewImageActivity.DEFAULT_IMG_PATH,
+                ViewImageActivity.DEFAULT_IMG_FILENAME);
+//        System.out.println(Environment.getExternalStorageDirectory().getAbsolutePath());
 
+        // Set the OnClickListener for Open Img Button
         btnOpenImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // The next step is sending the local img file info to the target activity
+                // I think the location string can meet the demand.
+                String local_img_path = ViewImageActivity.DEFAULT_IMG_PATH +
+                        ViewImageActivity.DEFAULT_IMG_FILENAME;
                 // 设置按钮点击后触发的 Activity 响应
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
-                intent.setType("image/*");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri img_uri = Uri.parse("file://"
+                        + ViewImageActivity.DEFAULT_IMG_PATH
+                        + ViewImageActivity.DEFAULT_IMG_FILENAME);
+                intent.setDataAndType(img_uri, "image/*");
                 startActivity(intent);
             }
         });
+    }
+
+    private void SaveImgToLocalPath(int ImgResID, String LocalPath, String LocalFileName) {
+        // 尝试将工程中的图片缓存到本地存储中，再将图片所在地址传递过去
+        // 还需要知道自带的图片浏览器接收图片资源的方式
+        // Withdraw the img resource in this project, then save it as a Bitmap Obj to local storage.
+        Bitmap img_res = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                ImgResID);
+
+        // Save the img file to the default local dir
+        File img_tmp = new File(LocalPath, LocalFileName);
+        try {
+            FileOutputStream fo = new FileOutputStream(img_tmp);
+            img_res.compress(Bitmap.CompressFormat.JPEG, 90, fo);
+            fo.flush();
+            fo.close();
+            // Send broadcast to System Image Viewer
+            Intent iv_intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri uri = Uri.fromFile(img_tmp);
+            iv_intent.setData(uri);
+            sendBroadcast(iv_intent);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/jikexuyuan/imageviewer/ViewImageActivity.java
+++ app/src/main/java/com/jikexuyuan/imageviewer/ViewImageActivity.java
@@ -19,39 +19,16 @@ import java.net.URL;
 
 public class ViewImageActivity extends Activity {
 
-    public static final String ACTION = "android.intent.action.VIEW";
     private ImageView imageView;
+    public static final String DEFAULT_IMG_PATH = "/storage/sdcard/Pictures/";
+    public static final String DEFAULT_IMG_FILENAME = "test.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_image);
 
-        Intent intent = getIntent();
-
-        if (intent.getAction().equals(Intent.ACTION_VIEW)) {
-            // test the intent from MainActivity
-            Toast.makeText(this, "No Images to Show!", Toast.LENGTH_SHORT).show();
-        } else {
-            imageView = (ImageView)findViewById(R.id.iv_ImageView);
-            Uri uri = intent.getData();
-            // 为获取图片文件的绝对路径做准备
-            // uri.getPath() 只能返回目录名，不含文件自身的名字
-            String proj[] = {MediaStore.Images.Media.DATA};
-            Cursor img_cursor = managedQuery(uri, proj, null, null, null);
-            int img_col_index = img_cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            img_cursor.moveToFirst();
-            String img_path = img_cursor.getString(img_col_index);
-            // 读取本地图片文件，并在 ImageView 中显示出来
-            Bitmap src_img = null;
-            File img_file = new File(img_path);
-            if (img_file.exists()) {
-                src_img = BitmapFactory.decodeFile(img_path);
-                imageView.setImageBitmap(src_img);
-            } else {
-                Toast.makeText(this, " This image file doesn't exist!", Toast.LENGTH_SHORT).show();
-            }
-        }
-
+        imageView = (ImageView)findViewById(R.id.iv_ImageView);
+        imageView.setImageURI(getIntent().getData());
     }
 }
